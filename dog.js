2e5471d2522a5a9d5632d2119ebce9760d3b0680
Gol1vf <71084774+Gol1vf@users.noreply.github.com>
2020-11-16 21:03:59 +0800

    change

---
+++ dog.js
@@ -0,0 +1,12 @@
+class Dog{
+    constructor(){
+        this.x = 150;
+        this.y = 200;
+        this.velo = 0;
+        this.width = 20;
+        this.height = 20;
+    }
+    fall(){
+
+    }
+}
