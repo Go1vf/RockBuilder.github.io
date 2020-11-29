cecc68a1e7cf9772b370f567977a35f5efc93c87
Gol1vf <71084774+Gol1vf@users.noreply.github.com>
2020-11-30 00:29:10 +0800

    change

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 var Head = document.getElementById('head');
-var Input = Head.getElementsByTagName('input')[0];
+var Main = document.getElementById('main');
+var Input = Head.getElementsByTagName('input')[0];//button
+var Span = Main.getElementsByTagName('span')[0];//Block:span
+var Alien = Main.getElementsByTagName('img')[0];//
+var Image = ['alien1.png', 'alien2.png', 'alien3.png'];
+var Speed = 1;
+Span.num = 0;
 function run() {
     Input.value = 'In Progress';
+    Input.disabled = false;
 }
