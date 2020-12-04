71881cc1eb8160443b3a2e2b66095bada1caa451
Gol1vf <71084774+Gol1vf@users.noreply.github.com>
2020-12-04 11:06:15 +0800

    final change

--- main.js
+++ main.js
@@ -3,42 +3,44 @@ var Input = format('upper').getElementsByTagName('input')[0];//button;
     var Alien = format('main').getElementsByTagName('img')[0];//The falling image;
     var Content = format('main').getElementsByTagName('div')[1];
     var Image = ['alien1.png', 'alien2.png', 'alien3.png'];
+    var Width = parseInt(getStyle(Content, 'width'));
+    var Height = parseInt(getStyle(Content, 'height')) - 50;
     var Speed = 1;
     var Ground = true;
     Span.num = 0;
-    function run() {
-        Input.value = '.....';
-        Input.disabled = true;
-        var Width = parseInt(getStyle(Content, 'width'));
-        var Height = parseInt(getStyle(Content, 'height')) - 50;
-        Alien.style.display = 'block';
-        function auto () {
+    function auto () {
             var random = Math.round(Math.random() * (Image.length - 1));
             var position = Math.round(Math.random() * (Width - 50));
             Alien.src = Image[random];
             Alien.style.left = position + 'px';
-            doMove(Alien, 'upper', Speed, Height, function() {
+            doMove(Alien, 'top', Speed, Height, function() {
                 if (Ground == true) {
                     Alien.style.top = '0px';
                     Span.num--;
                     Span.innerHTML = Span.num;
                     if (Span.num < 0) {
-                        input.value = 'Start';
-                        input.disabled = false;
+                        Input.value = 'Start';
+                        Input.disabled = false;
                         alert('Game is over');
                         Span.num = 0;
-                } 
-            }
-            else {
-                auto();
-            }
-        });
-    }
-    auto();
+                        Span.innerHTML = Span.num;
+                        Alien.style.display = "none";
+                    } else {
+                        auto();
+                    }
+                }
+            });
+        }
+    function run() {
+        Input.value = '.....';
+        Input.disabled = true;
+        Alien.style.display = 'block';
+        auto();
     }
     Alien.onclick = function() {
-    Span.num++;
-    Span.innerHTML = Span.num;
-    Speed = Speed+0.2*Math.LOG10E;
-    auto();
+        Span.num++;
+        Span.innerHTML = Span.num;
+        Speed = Speed+0.2*Math.LOG10E;
+        Alien.style.top = '0px';
+        auto();
     }
